02df956421940351cf6487fb729682a110483bb2
kjimenezc11 <kjimenezc@ctpplatanares.ed.cr>
2025-05-30 15:51:59 -0600

    entrega keneth cotidiano

---
+++ NOOTBOOK/modulo 2/2.6.9.py
@@ -0,0 +1,15 @@
+# ingresa un valor flotante para la variable a aquí
+a = 21
+# ingresa un valor flotante para la variable b aquí
+b=42
+
+# mostrar el resultado de la suma aquí
+print("el resultado de la suma es:", a + b)
+# mostrar el resultado de la resta aquí
+print("el resultado de la resta es:", a - b)
+# mostrar el resultado de la multiplicación aquí
+print("el resultado de la multiplicacion es:", a * b)
+# mostrar el resultado de la división aquí
+print("el resultado de la division es:", a / b)
+
+print("\n¡Eso es todo, amigos!")
